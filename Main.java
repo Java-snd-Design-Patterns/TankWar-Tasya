62ae27e51fa61b5aba35f0ed29ad2ddb9ccd4390
Aca <82041676+yoodingz@users.noreply.github.com>
2023-04-14 17:58:24 +0800

    Add files via upload
    Signed-off-by: Aca <82041676+yoodingz@users.noreply.github.com>

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        TankClient tankClient = new TankClient();
+        tankClient.mainlaunchFrame();
+    }
+}
---
+++ TankClient.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Yoodingz
+ */
+import java.awt.*;
+public class TankClient extends Frame{
+    public void mainlaunchFrame(){
+        // TODO code application logic here
+        this.setLocation(300,100);
+        this.setSize(800, 600);
+        this.setVisible(true);
+    }
+    
+}
